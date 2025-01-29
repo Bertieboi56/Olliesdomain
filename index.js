45723064f57f428301ed9ca013bbfbaa14d20cb5
Oliver <dirtybertie56@gmail.com>
2025-01-29 19:48:03 +0100

    done some changing to the front page, added typewrtier effect and a toast button!

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('#typed-text', {
+        strings: ["Welcome to Ollie's Domain!", "the best place in the world!","<(^_^)>"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        backDelay: 3000,
+        loop: true,
+        smartBackspace: true // This is a default setting
+    });
+});
+
+$(document).ready(function() {
     $("#liveToastBtn").click(function(){
         $("#liveToast").toast("show");
     });
